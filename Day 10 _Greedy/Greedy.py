346a4b5beb4892783eb02cfc6cc5634947e410ad
DayTeaJun <DayTeaJun>
2022-11-15 03:59:25 +0900

    Day10 update re

--- Day 10 _Greedy/Greedy.py
+++ Day 10 _Greedy/Greedy.py
@@ -1,42 +1,42 @@
-# # 동전 개수의 최솟값 구하기
-# N, K = map(int, input().split()) # N 동전 개수, K 목표 금액
-# A = [0] * N # 동전 데이터 리스트
+# 동전 개수의 최솟값 구하기
+N, K = map(int, input().split()) # N 동전 개수, K 목표 금액
+A = [0] * N # 동전 데이터 리스트
 
-# for i in range(N):
-#     A[i] = int(input())
+for i in range(N):
+    A[i] = int(input())
     
-# count = 0
+count = 0
 
-# # 가치가 큰 동전부터 선택해야 개수를 최소로 구성할 수 있음
-# for i in range(N - 1, -1, -1): # N -1 ~ 0으로 역순으로 반복
-#     if A[i] <= K: # 현재 K보다 동전 가치가 작거나 같으면
-#         count += int(K / A[i]) # 동전 수 += 목표 금액 / 현재 동전 가치
-#         K = K % A[i] # 목표 금액 = 목표 금액 % 현재 동전 가치
+# 가치가 큰 동전부터 선택해야 개수를 최소로 구성할 수 있음
+for i in range(N - 1, -1, -1): # N -1 ~ 0으로 역순으로 반복
+    if A[i] <= K: # 현재 K보다 동전 가치가 작거나 같으면
+        count += int(K / A[i]) # 동전 수 += 목표 금액 / 현재 동전 가치
+        K = K % A[i] # 목표 금액 = 목표 금액 % 현재 동전 가치
 
-# print(count) # 누적된 동전 수 출력
+print(count) # 누적된 동전 수 출력
 
-# # 카드 정렬하기 백준 1715번
-# from queue import PriorityQueue
-# N = int(input()) # 카드 묶음 개수
-# pq = PriorityQueue() # 우선순위 큐
+# 카드 정렬하기 백준 1715번
+from queue import PriorityQueue
+N = int(input()) # 카드 묶음 개수
+pq = PriorityQueue() # 우선순위 큐
 
-# for _ in range(N):
-#     date = int(input())
-#     pq.put(date)
+for _ in range(N):
+    date = int(input())
+    pq.put(date)
     
-# data1 = 0
-# data2 = 0
-# sum = 0
+data1 = 0
+data2 = 0
+sum = 0
 
-# # 자동 정렬에 따라 작은 카드 묶음 2개를 쉽게 뽑을 수 있음
-# while pq.qsize()>1:
-#     data1 = pq.get() # 2개 카드 묶음을 큐에서 뽑음
-#     data2 = pq.get()
-#     temp = data1 + data2 # 2개 카드 묶음을 합치는 데 필요한 비교 횟수를 결괏값에 더함
-#     sum += temp
-#     pq.put(temp) # 2개 카드 묶음의 합을 우선순위 큐에 다시 넣음
+# 자동 정렬에 따라 작은 카드 묶음 2개를 쉽게 뽑을 수 있음
+while pq.qsize()>1:
+    data1 = pq.get() # 2개 카드 묶음을 큐에서 뽑음
+    data2 = pq.get()
+    temp = data1 + data2 # 2개 카드 묶음을 합치는 데 필요한 비교 횟수를 결괏값에 더함
+    sum += temp
+    pq.put(temp) # 2개 카드 묶음의 합을 우선순위 큐에 다시 넣음
 
-# print(sum)
+print(sum)
 
 # 수를 묶어서 최댓값 만들기
 from queue import PriorityQueue
